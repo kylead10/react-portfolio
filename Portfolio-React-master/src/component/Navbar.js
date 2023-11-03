33a6c77fd9d06b40347420a4df77151a069c023c
kylead10 <adiokayode111@gmail.com>
2023-11-03 20:06:35 +0100

    spelling changes

--- Portfolio-React-master/src/component/Navbar.js
+++ Portfolio-React-master/src/component/Navbar.js
@@ -1,49 +1,68 @@
-import "./NavbarStyles.css"
-import React,{useState} from 'react'
-import {Link,NavLink} from "react-router-dom"
-import {FaBars,FaTimes} from "react-icons/fa";
-import {AiOutlineArrowRight} from "react-icons/ai";
-import Resume from '../assets/resume.pdf'
+import './NavbarStyles.css';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import Resume from '../assets/resume.pdf';
 
 const Navbar = () => {
-  const [click,setClick]=useState(false);
-  const handleClick=()=>setClick(!click);
- 
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
-    <div className="header">
-      <Link to="/"><h1>Portfolio</h1></Link>
-      <ul className={click ? "nav-menu.active" : "nav-menu"}>
+    <div className='header'>
+      <Link to='/'>
+        <h1>Portfolio</h1>
+      </Link>
+      <ul className={click ? 'nav-menu.active' : 'nav-menu'}>
         <li>
-            <NavLink to="/">About</NavLink>
+          <NavLink to='/'>About</NavLink>
         </li>
         <li>
-            <NavLink to="/education">Education</NavLink>
+          <NavLink to='/education'>Education</NavLink>
         </li>
         <li>
-            <NavLink to="/skills">Skills</NavLink>
+          <NavLink to='/skills'>Skills</NavLink>
         </li>
         <li>
-            <NavLink to="/project">Project</NavLink>
+          <NavLink to='/project'>Projects</NavLink>
         </li>
         <li>
-            <NavLink to="/SocialIcons">Social</NavLink>
+          <NavLink to='/SocialIcons'>Socials</NavLink>
         </li>
         <li>
-            <NavLink to="/contact">Contact</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
         </li>
         <li>
-            <Link to={Resume} className="btn-light" download="Harmanjot's Resume" target="_blank" rel="noreferrer">Resume<AiOutlineArrowRight size={20} style={{color:'#fff',marginLeft:"1rem",marginBottom:"-0.2rem" ,cursor:"pointer"}}/></Link>
+          <Link
+            to={Resume}
+            className='btn-light'
+            download="Harmanjot's Resume"
+            target='_blank'
+            rel='noreferrer'
+          >
+            Resume
+            <AiOutlineArrowRight
+              size={20}
+              style={{
+                color: '#fff',
+                marginLeft: '1rem',
+                marginBottom: '-0.2rem',
+                cursor: 'pointer',
+              }}
+            />
+          </Link>
         </li>
       </ul>
-      <div className="hamburger" onClick={handleClick}>
-        {
-          click ? 
-            (<FaTimes size={20} style={{color:"#fff"}}/>) :
-            <FaBars size={20} style={{color:"#fff"}}/>
-        }
+      <div className='hamburger' onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: '#fff' }} />
+        ) : (
+          <FaBars size={20} style={{ color: '#fff' }} />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
