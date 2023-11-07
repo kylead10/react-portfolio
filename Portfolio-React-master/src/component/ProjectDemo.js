dea9d1a1a327825b2057855ccea04376c2541a57
kylead10 <adiokayode111@gmail.com>
2023-11-07 15:25:25 +0100

    fixed project link

--- Portfolio-React-master/src/component/ProjectDemo.js
+++ Portfolio-React-master/src/component/ProjectDemo.js
@@ -94,7 +94,7 @@ const ProjectDemo = () => {
               <div className='connects'>
                 <Link
                   className='redirect'
-                  to='https://github.com/harmanjot2002/Task-Manager'
+                  to='https://github.com/'
                   target='_blank'
                 >
                   <FaGithub size={30} style={{ color: 'var(--bg-color' }} />
