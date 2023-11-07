e9dd44095bbcf59d18d672981dd63fa9884894c3
kylead10 <adiokayode111@gmail.com>
2023-11-07 15:21:59 +0100

    update

--- Portfolio-React-master/src/component/ProjectDemo.js
+++ Portfolio-React-master/src/component/ProjectDemo.js
@@ -88,7 +88,7 @@ const ProjectDemo = () => {
               <h4>Task Manager</h4>
               <p>
                 Task Manager Application built using HTML,CSS and JavaScript
-                where users can perform operationssuch set due dates,categorize
+                where users can perform operation such set due dates,categorize
                 and includes a calender view of tasks as needed.
               </p>
               <div className='connects'>
