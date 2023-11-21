c53c5b7f5c85438db5ffd6f73df53f5a8a16c56b
kylead10 <adiokayode111@gmail.com>
2023-11-21 21:48:49 +0100

    fixed typo

--- Portfolio-React-master/src/component/ProjectDemo.js
+++ Portfolio-React-master/src/component/ProjectDemo.js
@@ -91,8 +91,8 @@ const ProjectDemo = () => {
                 Uncover the mystery of time with my Age calculator app.
                 Effortlessly determine your age or the age of others by entering
                 birth-dates. Simplify your timekeeping and gain instant insights
-                into the passage of years. This can effortlessly calculate the
-                ages that shape your journey through time.
+                into the passage of years. This app can effortlessly calculate
+                the ages that shape your journey through time.
               </p>
               <div className='connects'>
                 <Link
