48841915b81d480ef62cc7541ca3f606e3e63ddf
kylead10 <adiokayode111@gmail.com>
2023-12-11 22:45:52 +0100

    add sign

--- Portfolio-React-master/src/component/SkillsSlider.js
+++ Portfolio-React-master/src/component/SkillsSlider.js
@@ -31,7 +31,7 @@ const SkillsSlider = () => {
                 </div>
                 <div className='progress'>
                   <h3>
-                    JavaScript<span>65%</span>
+                    JavaScript<span>67%</span>
                   </h3>
                   <div className='bar'>
                     <span></span>
